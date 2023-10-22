7c95c6a1197607d4e97919ce1a7c4aaf02a61f98
Asad Ullah Aziz <asadullahaziz99@gmail.com>
2023-10-22 15:20:24 +0500

    Fix: message processor

--- logdash_server/src/utils/msgProcessor.ts
+++ logdash_server/src/utils/msgProcessor.ts
@@ -4,9 +4,11 @@ import { eventEmitter } from './event.js';
 import { EVENTS } from '../constants.js';
 
 export async function messageProcessor(data: KafkaMessage) {
+    const value = JSON.parse(data.value?.toString("utf-8") as string);
+    
     try {
-        await redisClient.setEx(`api_action_log:${Date.now()}`, parseInt(process.env.CACHE_EXPIRY as string), JSON.stringify(data));
-        eventEmitter.emit(EVENTS.api_action_log_added, data);
+        await redisClient.setEx(`api_action_log:${Date.now()}`, parseInt(process.env.CACHE_EXPIRY as string), JSON.stringify(value));
+        eventEmitter.emit(EVENTS.api_action_log_added, value);
     } catch (error) {
         console.error(error);
     }
